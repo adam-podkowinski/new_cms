a9e289d0b24888cf11f12a89bb588fd4ec51bb75
adampodkdev@gmail.com <adampodkdev@gmail.com>
2021-10-16 22:02:41 +0200

    add eloquent rotes

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,9 +4,36 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Carbon;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * App\Models\Post
+ *
+ * @mixin Builder
+ * @property int $id
+ * @property string $title
+ * @property string $content
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property int $is_admin
+ * @method static Builder|Post newModelQuery()
+ * @method static Builder|Post newQuery()
+ * @method static Builder|Post query()
+ * @method static Builder|Post whereContent($value)
+ * @method static Builder|Post whereCreatedAt($value)
+ * @method static Builder|Post whereId($value)
+ * @method static Builder|Post whereIsAdmin($value)
+ * @method static Builder|Post whereTitle($value)
+ * @method static Builder|Post whereUpdatedAt($value)
+ */
 class Post extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    protected $date = ['deleted_at'];
+    protected $fillable = ['title', 'content', 'is_admin'];
 }
 
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,6 @@
 <?php
 
-/*
- * DATABASE RAW QUERIES
- */
-
+use App\Models\Post;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
@@ -11,8 +8,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+/*
+ * DATABASE RAW QUERIES
+ */
 
-Route::get('/insert/', function () {
+Route::get('/insert', function () {
     DB::insert('INSERT INTO posts(title, content) values(?, ?)', ['PHP with Laravel', 'Laravel is good']);
 });
 
@@ -26,13 +26,96 @@ Route::get('/read/{id}', function ($id) {
     return response()->json(['error' => 'Post with an id of 1 not found.'], 404);
 });
 
-Route::get('/update', function () {
+Route::get('/updateRaw', function () {
     $updated = DB::update('update posts set title=? where id=?', ['Updated title', 1]);
     return response()->json($updated);
 });
 
-Route::get('/delete', function () {
+Route::get('/deleteRaw', function () {
     $deleted = DB::delete('delete from posts where id = ?', [1]);
 
     return response()->json($deleted);
+});
+
+/*
+ * ELOQUENT
+ */
+
+Route::get('/findAll', function () {
+    $posts = Post::all();
+
+    foreach ($posts as $post) {
+        return response()->json($post);
+    }
+
+    return response()->json(['error' => 'error not found 404'], 404);
+});
+
+Route::get('/find', function () {
+    $post = Post::whereId(2)->get();
+
+    return response()->json($post);
+});
+
+Route::get('/findwhere', function () {
+    $post = Post::query()->where('title', 'PHP with Laravel')->orderBy('id', 'asc')->get();
+
+    return response()->json($post);
+});
+
+Route::get('/basicinsert', function () {
+    $post = new Post;
+    $post->title = 'new title';
+    $post->content = 'new content';
+    $post->save();
+});
+
+Route::get('/basicupdate/{id}', function ($id) {
+    $post = Post::whereId($id)->firstOrFail();
+    $post->title = 'new ' . $id;
+    $post->content = $id . ' content';
+    $post->save();
+});
+
+Route::get('/create', function () {
+    Post::query()->create(['title' => 'the title', 'content' => 'WOW Content', 'is_admin' => 1]);
+});
+
+
+Route::get('/update', function () {
+    Post::query()->where('is_admin', 0)->update(
+        [
+            'title' => 'idof2',
+            'content' => 'id2and is admin 0'
+        ]
+    );
+});
+
+Route::get('/deleteAll', function () {
+    $posts = Post::all();
+    foreach ($posts as $post) {
+        $post->delete();
+    }
+});
+
+Route::get('/delete', function () {
+    Post::destroy(7);
+});
+
+Route::get('/deleteMany', function () {
+    Post::destroy([4, 5]);
+});
+
+Route::get('/softdelete', function () {
+    Post::query()->findOrFail(3)->deleteOrFail();
+});
+
+Route::get('/readsoftdeleted', function () {
+// WRONG
+//    $post = Post::query()->find(1);
+    $post = Post::withTrashed()->where('id', 1)->get();
+
+    $user = new \App\Models\User();
+
+    return response()->json($post);
 });
